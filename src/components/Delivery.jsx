40bd35e98364fdf455dfec0f326524fe323923b5
Emshtml <casadathereza1@gmail.com>
2026-05-20 12:56:42 -0300

    Create Delivery.jsx

---
+++ src/components/Delivery.jsx
@@ -0,0 +1,16 @@
+function Delivery() {
+  return (
+    <section
+      className="delivery"
+      id="delivery"
+    >
+      <h2>Delivery em São Paulo</h2>
+
+      <p>
+        Entrega rápida e pizzas sempre quentes.
+      </p>
+    </section>
+  )
+}
+
+export default Delivery
