37dc5eefd9e931458cc16804374beafbc920e59d
Emshtml <casadathereza1@gmail.com>
2026-05-20 12:47:18 -0300

    Add pizzas data to pizzas.js

---
+++ src/data/pizzas.js
@@ -0,0 +1,30 @@
+const pizzas = [
+  {
+    id: 1,
+    name: 'Calabresa Premium',
+    description: 'Calabresa artesanal e queijo especial.',
+    price: 'R$ 49,90',
+    image:
+      'https://images.unsplash.com/photo-1513104890138-7c749659a591'
+  },
+
+  {
+    id: 2,
+    name: 'Frango Catupiry',
+    description: 'Frango desfiado com catupiry cremoso.',
+    price: 'R$ 54,90',
+    image:
+      'https://images.unsplash.com/photo-1594007654729-407eedc4be65'
+  },
+
+  {
+    id: 3,
+    name: 'Quatro Queijos',
+    description: 'Mistura premium de quatro queijos.',
+    price: 'R$ 59,90',
+    image:
+      'https://images.unsplash.com/photo-1548365328-9f547fb0953b'
+  }
+]
+
+export default pizzas
