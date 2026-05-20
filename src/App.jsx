0c65ca9ec4f22b34fdcb6b1cf488e8bb52d736da
Emshtml <casadathereza1@gmail.com>
2026-05-20 12:46:17 -0300

    Create App component with multiple sections

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import MenuSection from './components/MenuSection'
+import Delivery from './components/Delivery'
+import Footer from './components/Footer'
+import WhatsappButton from './components/WhatsappButton'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <MenuSection />
+      <Delivery />
+      <Footer />
+      <WhatsappButton />
+    </>
+  )
+}
+
+export default App
