383430ad124e8dda314245225206c00094834387
Emshtml <casadathereza1@gmail.com>
2026-05-20 12:50:04 -0300

    Add Header component for Pizzaria Premium

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pizzaria Premium</h1>
+
+      <nav>
+        <a href="#menu">Cardápio</a>
+        <a href="#delivery">Delivery</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
