389e857994bc3e39c441a2973ac6324e076b570f
Emshtml <casadathereza1@gmail.com>
2026-05-20 13:00:08 -0300

    Add WhatsappButton component for WhatsApp link

---
+++ src/components/WhatsappButton.jsx
@@ -0,0 +1,13 @@
+function WhatsappButton() {
+  return (
+    <a
+      href="https://wa.me/5511999999999"
+      className="whatsapp-button"
+      target="_blank"
+    >
+      WhatsApp
+    </a>
+  )
+}
+
+export default WhatsappButton
