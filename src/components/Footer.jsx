646c8a4f178eec4572238575f570e5116688e3ba
Emshtml <casadathereza1@gmail.com>
2026-05-20 12:58:14 -0300

    Add Footer component with copyright notice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © 2026 Pizzaria Premium - Todos os direitos reservados
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
